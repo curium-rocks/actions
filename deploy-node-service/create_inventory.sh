619ca2a3d1f28293ca6608039f0d9d67053d1f7d
Andrew Boyd <8363252+bryopsida@users.noreply.github.com>
2021-09-06 18:51:16 -0500

    update format

--- deploy-node-service/create_inventory.sh
+++ deploy-node-service/create_inventory.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 json_str=$1
+for row in $(echo "${json_str}" | jq -r '.[] | @base64'); do
+    _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+    }
+   echo $(_jq '.hostname')  ansible_connection=$(_jq '.connection') ansible_user=$(_jq '.username')
+done
+
+echo ''
 echo '[services]'
 for row in $(echo "${json_str}" | jq -r '.[] | @base64'); do
     _jq() {
