7f650c684a94c9447ceaf01fd9baa58cbd8f5087
Andrew Boyd <8363252+bryopsida@users.noreply.github.com>
2021-09-06 18:14:01 -0500

    add create inventory step

---
+++ deploy-node-service/create_inventory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+json_str=$1
+echo '[services]'
+for row in $(echo "${json_str}" | jq -r '.[] | @base64'); do
+    _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+    }
+   echo $(_jq '.hostname')  ansible_connection=$(_jq '.connection') ansible_user=$(_jq '.username')
+done
