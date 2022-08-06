bbf180cb9bf39f4efaff7ced63814c2c37ef2942
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-08-06 08:51:13 -0500

    initial setup of action

---
+++ wireguard/src/main.ts
@@ -0,0 +1,12 @@
+
+import * as core from '@actions/core'
+
+async function run(): Promise<void> {
+  try {
+
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run()
